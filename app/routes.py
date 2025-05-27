fc243e6f0b7d0f90416a3f778eb85a0511e4a464
Kibetts <kibetsbrian@gmail.com>
2025-05-27 23:19:16 +0300

    create edit profile route

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from app import app, db
 from flask import render_template, flash, redirect, url_for, request
 from urllib.parse import urlsplit
-from app.forms import LoginForm, RegistrationForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
 from flask_login import current_user, login_user, logout_user, login_required
 import sqlalchemy as sa
 from app.models import User
@@ -58,6 +58,7 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
+
 @app.route('/user/<username>')
 @login_required
 def user(username):
@@ -67,3 +68,18 @@ def user(username):
         {'author': user, 'body': 'Test 2'}
     ]
     return render_template('user.html', user=user, posts=posts)
+
+@app.route('/edit_profile', methpds=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm
+    if form.validate_on_submit:
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved successfully')
+        return url_for('edit_profile')
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile', form=form)
