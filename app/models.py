20dc5f9769eb37d1df0ebb5197654ab8de6903f3
Kibetts <kibetsbrian@gmail.com>
2025-07-17 20:20:36 +0300

    add follow functions

--- app/models.py
+++ app/models.py
@@ -21,7 +21,6 @@ class User(UserMixin, db.Model):
     about_me: so.Mapped[Optional[str]] = so.mapped_column(sa.String(140))
     last_seen: so.Mapped[Optional[datetime]] = so.mapped_column(default=lambda: datetime.now(timezone.utc))
 
-    # many to many followers r/ship
 
     followers = sa.Table(
     'followers',
@@ -50,9 +49,34 @@ class User(UserMixin, db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
     
-    def avatar (self, size):
+    def avatar(self, size):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
+    
+
+    def follow(self, user):
+        if not self.is_following(user):
+            self.following.add(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.following.remove(user)
+
+    def is_following(self, user):
+        query = self.following.select().where(User.id == user.id)
+        return db.session.scalar(query) is not None
+    
+    def followers_count(self):
+        query = sa.select(sa.func.count()).select_from(
+            self.followers.select().subquery())
+        return db.session.scalar(query)
+    
+    def following_count(self):
+        query = sa.select(sa.func.count()). select_from(
+            self.following.select().subquery())
+        return db.session.scalar(query)
+
+
 
     
 
